af846695949bfd55049f233bad15a7c7b3593cf1
sadief <sadiefreeman@gmail.com>
2019-04-02 11:41:30 -0700

    Add tests for different languages

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,36 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Lassie")
-	want := "Hello, Lassie"
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 	}
+
+	t.Run("in Italian", func(t *testing.T) {
+		got := Hello("Andrea", "Italian")
+		want := "Bonjourno, Andrea"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Sophie", "French")
+		want := "Bonjour, Sophie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Lassie", "English")
+		want := "Hello, Lassie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("", "English")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
--- hello/main.go
+++ hello/main.go
@@ -2,11 +2,36 @@ package main
 
 import "fmt"
 
+const englishPrefix = "Hello, "
+const italianPrefix = "Bonjourno, "
+const frenchPrefix = "Bonjour, "
+
 // Hello ...
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %v", name)
+func Hello(name, language string) string {
+	var prefix string
+
+	switch language {
+	case "English":
+		prefix = englishPrefix
+		if name == "" {
+			name = "World"
+		}
+	case "Italian":
+		prefix = italianPrefix
+		if name == "" {
+			name = "Mondo"
+		}
+	case "French":
+		prefix = frenchPrefix
+		if name == "" {
+			name = "Monde"
+		}
+
+	}
+
+	return fmt.Sprintf("%v%v", prefix, name)
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
